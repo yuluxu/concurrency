b7ede254ce84b2d977f5ff44cc3582a39da41b85
yuluxu <yuluxu86@yahoo.com.cn>
2013-01-25 21:12:13 +0800

    增加错误的同步示例

---
+++ src/main/java/com/yuluxu/concurrency/ch3/NoVisibility.java
@@ -0,0 +1,27 @@
+package com.yuluxu.concurrency.ch3;
+
+public class NoVisibility
+{
+	private static boolean ready;
+	private static int number;
+
+	private static class ReaderThread extends Thread
+	{
+		@Override
+		public void run()
+		{
+			while (!ready)
+			{
+				Thread.yield();
+			}
+			System.out.println(number);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		new ReaderThread().start();
+		number = 42;
+		ready = true;
+	}
+}
